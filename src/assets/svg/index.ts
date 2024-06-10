2cbb5cbdd7928ec194fe149eb63a989a5638b792
dmittryyyy <dbolobchenko@corp.local>
2024-06-10 21:03:03 +0700

    customize svg display

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,2 @@
+export { default as SnakeIcon } from './Snake.svg';
+
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
+import {  SnakeIcon } from '../../assets/svg'
+
 const Header = () => {
     return (
         <header className='flex py-8 items-center justify-center gap-x-4 w-full bg-blue-100 text-4xl font-bold'>
             <h1 className=''>Snake Game</h1>
-            иконка
+            <SnakeIcon height={64} width={64} />
         </header>
     );
 };
